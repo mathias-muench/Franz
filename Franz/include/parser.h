488a199c7632c3d02edb2254e186a7f110a43055
Thomas Lauf <thomas.lauf@tngtech.com>
2016-03-11 11:22:43 +0100

    Add parser skeleton

---
+++ Franz/include/parser.h
@@ -0,0 +1,13 @@
+#ifndef INC_PARSER_H_
+#define INC_PARSER_H_
+
+#include <sys/types.h>
+
+typedef struct {
+	char* name;
+	time_t timestamp;
+} name_time_t;
+
+void parse_file(char* filename, name_time_t* result);
+
+#endif /* INC_PARSER_H_ */
---
+++ Franz/src/parser.c
@@ -0,0 +1,6 @@
+#include "parser.h"
+
+void parse_file(char* filename, name_time_t* result) {
+	
+}
+
