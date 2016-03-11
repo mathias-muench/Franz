b567ae5c9d7955c79e13bcd83838392f0774f491
Mathias Muench <mathias.muench@tngtech.com>
2016-03-11 08:05:32 -0500

    Change time aendern geht

--- Franz/test/normfile.c
+++ Franz/test/normfile.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <time.h>
 
 #include "normfile.h"
 
 int main(int argc, char *argv[]) {
-	doit("test/foo", "test/bar", NULL);
+	struct utimbuf times;
+	times.actime = time(NULL);
+	times.modtime = time(NULL) - 3600;
+	doit("test/foo", "test/bar", &times);
 }
