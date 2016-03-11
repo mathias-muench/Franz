eee1a0b87ab335971ca7b29a9260a417417d31bb
Mathias Muench <mathias.muench@tngtech.com>
2016-03-11 10:32:20 -0500

    Simple integration test with hardcoded values

--- Franz/src/franz.c
+++ Franz/src/franz.c
@@ -1,3 +1,27 @@
-int main(int argc, char *argv[]) {
-	return 0;
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "readdir.h"
+#include "normfile.h"
+
+void doit(char *filename)
+{
+    char new_filename[FILENAME_MAX];
+
+    strcpy(new_filename, filename);
+    strcat(new_filename, "+");
+    normfile(filename, new_filename, time(NULL) - 3600);
+    printf("FILE: %s\n", filename);
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2)
+    {
+        printf("Parameter directory missing.\n");
+        exit(1);
+    }
+    printf ("Reading entries of %s\n\n", argv[1]);
+
+    read_dir(argv[1], doit);
 }
